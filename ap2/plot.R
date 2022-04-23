07b58c060550205c9683600643f1064859a9d65a
jose <macaverde604@gmail.com>
2022-04-23 20:26:06 -0300

    atualiza metodo de plot de novos dados

--- ap2/plot.R
+++ ap2/plot.R
@@ -2,9 +2,9 @@ rm(list = ls())
 
 data <- read.csv(file = 'data/trans.csv', header = FALSE)
 
-X <- data[, -ncol(data)]
+X <- as.matrix(data[, -ncol(data)])
 Y <- data[, ncol(data)]
 
 png(file = 'fig/plot-trans.png', width = 600, height = 400)
-plot(X, type = 'p', col = Y, xlim = c(-0.5, 1.5), ylim = c(5, 15))
+plot(X, type = 'p', col = Y, xlim = c(min(X[, 1]), max(X[, 1])), ylim = c(min(X[, 2]), max(X[, 2])))
 dev.off()
