6220a3fcae71a6c1d81809a6f27959f1492f9536
jose <macaverde604@gmail.com>
2022-04-23 20:01:34 -0300

    modifica metodo de otimizaçao para plot da loss

--- ap2/otm.py
+++ ap2/otm.py
@@ -9,6 +9,7 @@ Created on Sat Apr 23 18:01:34 2022
 import numpy as np
 import pandas as pd
 from sklearn.metrics import silhouette_score, silhouette_samples
+import matplotlib.pyplot as plt
 
 def silh(L):
     return silhouette_score(X = np.dot(L, X.T).T, labels = Y)
@@ -24,11 +25,13 @@ def grad(target, L0, delta = 0.001):
     return grad
 
 def otm(target, L0, lr = 1, MAX_EPOCAS = 100, delta = 0.001):
+    log = []
     for epoca in range(MAX_EPOCAS):
+        log.append(target(L0))
         dL = grad(target = target, L0 = L0, delta = delta)
         L0 = L0 - lr * dL
-        # print(target(L0))
-    return L0
+    log.append(target(L0))
+    return L0, log
 
 # read data
 data = pd.read_csv('data/raw.csv', header = None).values
@@ -37,9 +40,14 @@ X = data[:, 0:-1]
 Y = data[:, -1]
 
 # otm
-L0 = np.eye(len(X[0, :]))
-L = otm(silh, L0, lr = -1)
+# L0 = np.eye(len(X[0, :]))
+L0 = np.random.randn(len(X[0, :]), len(X[0, :]))
+L, log = otm(silh, L0, lr = -1, MAX_EPOCAS = 100)
 
-# write
-X = np.dot(L, X.T).T
-pd.DataFrame(np.concatenate((X, Y.reshape(-1, 1)), axis = 1)).to_csv('data/trans.csv', header = None, index = None)
+# plot
+plt.plot(log)
+plt.savefig('fig/loss.png')
+
+# # write
+# X = np.dot(L, X.T).T
+# pd.DataFrame(np.concatenate((X, Y.reshape(-1, 1)), axis = 1)).to_csv('data/trans.csv', header = None, index = None)
