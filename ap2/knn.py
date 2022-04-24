f5cea3f7807a3b96d6a48d12e05cb3933a9ec685
jose <macaverde604@gmail.com>
2022-04-24 10:48:28 -0300

    modifica metodo knn para varredura

--- ap2/knn.py
+++ ap2/knn.py
@@ -15,26 +15,30 @@ K = 10
 size = 200
 step = round(size / K)
 
-log = []
-
-for k in range(K):
-    # read
-    data = pd.read_csv('data/kfold/trans-' + str(k) + '.csv', header = None).values
-    X = data[:, 0:-1]
-    Y = data[:, -1]
-
-    # train/test
-    X_train = X[step:, :]
-    Y_train = Y[step:]
-    X_test = X[:step, :]
-    Y_test = Y[:step]
-
-    # knn
-    model = KNeighborsClassifier(n_neighbors = 3)
-    model.fit(X_train, Y_train)
-
-    # log
-    acc = model.score(X_test, Y_test)
-    log.append(acc)
-
-print(np.mean(log), np.std(log))
+loglog = []
+for n in range(15):
+    n += 1
+    log = []
+    
+    for k in range(K):
+        # read
+        data = pd.read_csv('data/kfold/trans-' + str(k) + '.csv', header = None).values
+        X = data[:, 0:-1]
+        Y = data[:, -1]
+    
+        # train/test
+        X_train = X[step:, :]
+        Y_train = Y[step:]
+        X_test = X[:step, :]
+        Y_test = Y[:step]
+    
+        # knn
+        model = KNeighborsClassifier(n_neighbors = n)
+        model.fit(X_train, Y_train)
+    
+        # log
+        acc = model.score(X_test, Y_test)
+        log.append(acc)
+    
+    print(n, np.mean(log), np.std(log))
+    loglog.append(np.mean(log))
