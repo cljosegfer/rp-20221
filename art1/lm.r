24b51c3f670e42eeb93082151a69485a969a6fac
jose <macaverde604@gmail.com>
2022-05-15 15:09:32 -0300

    adiciona script r para relaçao

---
+++ art1/lm.r
@@ -0,0 +1,20 @@
+rm(list=ls())
+
+# read
+metodo <- 'base'
+auc_d <- read.csv(file = sprintf('output/%s/coletivo.csv', metodo))
+dis_d <- read.csv(file = sprintf('output/%s/d_coletivo.csv', metodo))
+silh <- read.csv(file = 'output/otm-log.csv', header = FALSE)
+metodo <- 'silh'
+auc_s <- read.csv(file = sprintf('output/%s/coletivo.csv', metodo))
+dis_s <- read.csv(file = sprintf('output/%s/d_coletivo.csv', metodo))
+
+# lm
+auc <- auc_s[, 2] - auc_d[, 2]
+silh <- silh[, 3] - silh[, 2]
+dis <- dis_s[, 2] - dis_d[, 2]
+
+# plot
+plot(silh, auc)
+plot(dis, auc)
+plot(silh, dis)
